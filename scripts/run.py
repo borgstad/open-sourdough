dc7fca331f0172795176b9d2100cab82d8ce5a5b
Andreas Borgstad <aborgstad@gmail.com>
2023-03-04 21:09:28 +0100

    fix: use the session function, instead of implementing it

--- scripts/run.py
+++ scripts/run.py
@@ -1,12 +1,15 @@
+import datetime
 import time
 
 import open_sourdough_monitor.main
 
 
 def run_every_min():
-    while True:
-        open_sourdough_monitor.main.SourDoughMonitor().take_picture()
-        time.sleep(60)
+    session_time = datetime.timedelta(minutes=1)
+    picture_interval = datetime.timedelta = datetime.timedelta(seconds=5)
+    open_sourdough_monitor.main.SourDoughMonitor(
+        session_time=session_time, picture_interval=picture_interval
+    ).start_session()
 
 
 if __name__ == "__main__":
