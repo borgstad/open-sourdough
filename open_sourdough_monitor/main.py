b4ee490d60104dd997d7535cbccebac3746220c4
Andreas Borgstad <aborgstad@gmail.com>
2023-03-04 21:08:12 +0100

    fix: created dir instead of cv2 file

--- open_sourdough_monitor/main.py
+++ open_sourdough_monitor/main.py
@@ -15,20 +15,22 @@ class SourDoughMonitor:
         self.session_id = models.get_latest_id() + 1
         self.session_end_time = datetime.datetime.now() + session_time
         self.picture_interval = picture_interval
+        self.abs_filepath = settings.OPEN_SOURDOUGH_ROOT_IMAGE_DIR / str(
+            self.session_id
+        )
 
     def start_session(self):
+        self.abs_filepath.mkdir(parents=True, exist_ok=True)
         while datetime.datetime.now() < self.session_end_time:
             self.take_picture()
-            time.sleep(self.picture_interval.seconds)
+            time.sleep(self.picture_interval.total_seconds())
 
     def take_picture(self):
         camera = cv2.VideoCapture(0)
         _, image = camera.read()
         now = datetime.datetime.utcnow()
         filename = now.strftime("%Y-%m-%d_%H-%M-%S-%f") + ".jpg"
-        abs_filepath = settings.OPEN_SOURDOUGH_ROOT_IMAGE_DIR / str(self.session_id) / filename
-        abs_filepath.mkdir(parents=True, exist_ok=True)
-        cv2.imwrite(str(abs_filepath), image)
+        cv2.imwrite(str(self.abs_filepath / filename), image)
         with models.session_factory() as session:
             session.add(models.SourDoughImages(filename, now, self.session_id))
             session.commit()
